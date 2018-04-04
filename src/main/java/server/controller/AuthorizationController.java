714f4316ed0914a0125f22f9919a282d95f6f827
Thewhiterabbit123 <nivasilieva13@gmail.com>
2018-04-04 23:57:53 +0300

    userServiceTest add

--- src/main/java/server/controller/AuthorizationController.java
+++ src/main/java/server/controller/AuthorizationController.java
@@ -169,15 +169,17 @@ public class AuthorizationController {
 
         User userInDB = userService.checkUserById(userId);
         String userLogin = userInDB.getLogin();
-        String userImage = userInDB.getImage();
 
         if (userLogin == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new Message(MessageStates.UNAUTHORIZED.getMessage()));
         }
 
+        String userImage = userInDB.getImage();
+        String userEmail = userInDB.getEmail();
         User user = new User();
         user.setLogin(userLogin);
         user.setImage(userImage);
+        user.setEmail(userEmail);
 
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(user);
 
--- src/test/java/server/services/UserServiceTest.java
+++ src/test/java/server/services/UserServiceTest.java
@@ -5,9 +5,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+import server.model.User;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@Transactional
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class UserServiceTest {
@@ -17,29 +23,67 @@ class UserServiceTest {
 
     @Test
     void getAllUsers() {
+        User user1 = new User("testlogin", "testemail@mail.ru", "password");
+        User user2 = new User("testlogin1", "testemail1@mail.ru", "password1");
+        userService.addUser(user1);
+        userService.addUser(user2);
+        List<User> users;
+        users = userService.getAllUsers();
+        User settedUser1 = users.get(users.size()-2);
+        User settedUser2 = users.get(users.size()-1);
+        assertEquals(settedUser1.getLogin(), user1.getLogin());
+        assertEquals(settedUser1.getEmail(), user1.getEmail());
+        assertEquals(settedUser1.getPassword(), user1.getPassword());
+        assertEquals(settedUser2.getLogin(), user2.getLogin());
+        assertEquals(settedUser2.getEmail(), user2.getEmail());
+        assertEquals(settedUser2.getPassword(), user2.getPassword());
     }
 
     @Test
     void addUser() {
+        User newUser = new User("testname", "testemail@mail.ru", "testpassword");
+        Integer id = userService.addUser(newUser);
+        assertTrue(id > 0);
     }
 
     @Test
     void getUserById() {
+        User newUser = new User("testname", "testemail@mail.ru", "testpassword");
+        Integer id = userService.addUser(newUser);
+        User settedUser = userService.getUserById(id);
+        assertEquals(newUser.getLogin(), settedUser.getLogin());
+        assertEquals(newUser.getEmail(), settedUser.getEmail());
+        assertEquals(newUser.getPassword(),settedUser.getPassword());
     }
 
     @Test
     void isEmailRegistered() {
+        User newUser = new User("testname", "testemail@mail.ru", "testpassword");
+        Integer id = userService.addUser(newUser);
+        User settedUser = userService.getUserById(id);
+        assertTrue(userService.isEmailRegistered(settedUser.getEmail()));
     }
 
     @Test
     void isLoginRegistered() {
+        User newUser = new User("testname", "testemail@mail.ru", "testpassword");
+        Integer id = userService.addUser(newUser);
+        User settedUser = userService.getUserById(id);
+        assertTrue(userService.isLoginRegistered(settedUser.getLogin()));
     }
 
     @Test
     void authorizeUser() {
+        User newUser = new User("testname", "testemail@mail.ru", "testpassword");
+        Integer idNewUser = userService.addUser(newUser);
+        Integer idFromAuthorization = userService.authorizeUser(newUser);
+        assertEquals(idNewUser, idFromAuthorization);
     }
 
     @Test
     void checkUserById() {
+        User newUser = new User("testname", "testemail@mail.ru", "testpassword");
+        Integer idNewUser = userService.addUser(newUser);
+        assertNotNull(userService.checkUserById(idNewUser));
     }
 }
