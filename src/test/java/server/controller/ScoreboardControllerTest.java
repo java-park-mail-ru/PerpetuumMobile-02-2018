3eebc76b0f7183bdba4ee890183b1edf8b0476d6
Tim <garif-tim@yandex.ru>
2018-04-10 00:45:03 +0300

    change ScoreBoardControllerTest

--- src/test/java/server/controller/ScoreboardControllerTest.java
+++ src/test/java/server/controller/ScoreboardControllerTest.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 @AutoConfigureMockMvc(print = MockMvcPrint.NONE)
 class ScoreboardControllerTest {
 
