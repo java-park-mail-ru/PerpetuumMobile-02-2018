81d501254c5ca65f0182774a1548d66f7e8501ca
Tim <garif-tim@yandex.ru>
2018-05-25 02:02:20 +0300

    delete unused import

--- src/main/java/server/controller/AuthorizationController.java
+++ src/main/java/server/controller/AuthorizationController.java
@@ -1,6 +1,5 @@
 package server.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
